9461a6170d19a57649185624565f0bd369ebe18e
Valentina Arancibia <valentina.arancibia@genstudents.org>
2025-02-11 16:08:07 -0300

    Eliminacion de archivos malos controlador y ruta

--- backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createReservation } = require("../controllers/userController");
-
-router.post("/reservations", createReservation);  // Crear una nueva reservacion para añadir a la tabla
-
-
-
-module.exports = router;
