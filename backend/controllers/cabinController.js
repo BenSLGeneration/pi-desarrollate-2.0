bb0a6ebd39ca654c3e6eee73b44b16eb930b743f
Valentina Arancibia <valentina.arancibia@genstudents.org>
2025-02-13 10:37:53 -0300

    cabin controller & routes

---
+++ backend/controllers/cabinController.js
@@ -0,0 +1,32 @@
+const Cabin = require("../models/Cabin");
+
+// Obtener todos los datos de cabinas
+
+
+
+
+
+
+// Crear nuevos datos cabina
+
+
+
+
+
+
+
+// Obtener cabina especifica por Id
+
+
+
+
+
+
+// Actualizar datos cabina
+
+
+
+
+
+
+// Eliminar datos cabina
---
+++ backend/routes/cabinRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const cabinController = require("../controllers/cabinController");
+
+// Rutas para peticiones 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
