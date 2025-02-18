8eb2c6a6dd8c0f7233f84ce79f4ccb52f5a5e68b
Valentina Arancibia <valentina.arancibia@genstudents.org>
2025-02-18 15:27:21 -0300

    cambios en componentes dashboard

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -16,7 +16,7 @@ const AdminPanel = () => {
                 <div className="d-flex justify-content-between align-items-center mb-3">
                     <h5 className="mb-0 fw-bold">Panel de Administracion</h5>
                 </div>
-                <div style={{ maxHeight: '300px', overflowY: 'auto' }}> {
+                <div style={{ maxHeight: '300px', maxWidth: 'auto', overflowY: 'auto' }}> {
                 }
                     {users.map((user) => (
                         <div key={user.id}>
