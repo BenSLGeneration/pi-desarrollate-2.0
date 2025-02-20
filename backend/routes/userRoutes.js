3490d7726865b2e244f83b0835eb7f67c0a9ccdd
BenSLGeneration <benjamin.salome@genstudents.org>
2025-02-20 10:01:50 -0300

    cambios antes de hacer crud

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { createUser, getUsers, getUserById, updateUser, deleteUser, loginUser } = require("../controllers/userController");
-const authJWT = require("../middleware/auth")
-const isAdmin = require("../middleware/isAdmin")
+const authJWT = require("../middleware/auth");
+const isAdmin = require("../middleware/isAdmin");
 
 router.post("/", createUser);
-router.get("/", authJWT, isAdmin, getUsers);
+router.get("/", getUsers);
 router.get("/:id", getUserById);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.post("/:id", loginUser)
+router.put("/:id", authJWT, updateUser);
+router.delete("/:id", authJWT, deleteUser);
+router.post("/:id", loginUser);
 
 module.exports = router;
